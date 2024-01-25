1158a19cf038d026011f5655001c886f84a6e5ff
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-01-25 23:41:54 +0300

    institutionaluser

---
+++ BuySell.Models/Concrete/InstitutionalUser.cs
@@ -0,0 +1,18 @@
+﻿using BuySell.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell.Models.Concrete
+{
+    public class InstitutionalUser : BaseModel
+    {
+        public string? CorporateName { get; set; }
+        public string? TaxAdministration { get; set; }
+        public int TaxNumber { get; set; }
+        public string? GSMNo { get; set; }
+        public string? Address { get; set; }
+    }
+}
