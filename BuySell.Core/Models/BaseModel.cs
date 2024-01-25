24af968bf9fcaad0cc09e6985266af345bb2429a
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-01-25 23:32:35 +0300

    basemodels

---
+++ BuySell.Core/Models/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell.Core.Models
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+
+        private DateTime _createdDate = DateTime.Now;
+        public DateTime CreatedDate {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
