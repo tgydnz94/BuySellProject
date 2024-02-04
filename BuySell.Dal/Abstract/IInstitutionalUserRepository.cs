7e81eecacd86b638d7b076e07fe0bc5029a22e9b
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-05 01:22:35 +0300

    InstitutionalUserDal

---
+++ BuySell.Dal/Abstract/IInstitutionalUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using BuySell.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell.Dal.Abstract
+{
+    public interface IInstitutionalUserRepository
+    {
+        public void Add(InstitutionalUser ınstitutionalUser);
+        public void Update(InstitutionalUser ınstitutionalUser);
+        public void Delete(InstitutionalUser ınstitutionalUser);
+        List<InstitutionalUser> GetAll();
+        InstitutionalUser GetOne(Expression<Func<InstitutionalUser, bool>> filter);
+    }
+}
---
+++ BuySell.Dal/Concrete/InstitutionalUserRepository.cs
@@ -0,0 +1,49 @@
+﻿using BuySell.Dal.Abstract;
+using BuySell.Dal.Context;
+using BuySell.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuySell.Dal.Concrete
+{
+    public class InstitutionalUserRepository : IInstitutionalUserRepository
+    {
+        private readonly BuySellContext _context;
+
+        public InstitutionalUserRepository(BuySellContext context) 
+        {
+            _context = context;
+        }
+        public void Add(InstitutionalUser ınstitutionalUser)
+        {
+            _context.Add(ınstitutionalUser);
+            _context.SaveChanges();
+        }
+
+        public void Delete(InstitutionalUser ınstitutionalUser)
+        {
+            _context.Remove(ınstitutionalUser);
+            _context.SaveChanges();
+        }
+
+        public List<InstitutionalUser> GetAll()
+        {
+            return _context.Set<InstitutionalUser>().ToList();
+        }
+
+        public InstitutionalUser GetOne(Expression<Func<InstitutionalUser, bool>> filter)
+        {
+            return  _context.Set<InstitutionalUser>().SingleOrDefault(filter);
+        }
+
+        public void Update(InstitutionalUser ınstitutionalUser)
+        {
+            _context.Update(ınstitutionalUser);
+            _context.SaveChanges();
+        }
+    }
+}
